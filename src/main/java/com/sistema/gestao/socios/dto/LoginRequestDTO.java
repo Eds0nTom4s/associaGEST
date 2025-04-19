5fbbc545004fd37fd0ac2858c5cdd15432a7f268
Edson Tomas <edsonhacker13@gmail.com>
2025-04-19 16:39:39 +0100

    adicionamos validacao no campo de email na classe LogginRequestDTO

--- src/main/java/com/sistema/gestao/socios/dto/LoginRequestDTO.java
+++ src/main/java/com/sistema/gestao/socios/dto/LoginRequestDTO.java
@@ -1,5 +1,7 @@
 package com.sistema.gestao.socios.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +12,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequestDTO {
+    @NotBlank(message = "Email não pode ser vazio")
+    @Email(message = "Formato de email inválido")
     private String email;
     private String senha;
 }
